64ea369c33e2752a9a33e65b9caeac0722c9baec
fbhood <mail@fabiopacifici.com>
2023-10-03 12:32:58 +0200

    add AppHeader and SearchBox components

---
+++ ex-boolflix/src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="d-flex justify-content-between p-3">
+
+      <div class="logo text-danger">
+        FABIOFLIX
+      </div>
+
+      <SearchBox @perform-search="getData"></SearchBox>
+
+
+    </header>
+
+</template>
+
+<script>
+import SearchBox from "./SearchBox.vue";
+import {state} from '../state.js';
+  export default {
+    name: 'AppHeader',
+    components:{
+      SearchBox
+    },
+    data(){
+      return {
+        state
+      }
+    },
+    methods: {
+      getData() {
+      this.state.fetchResults();
+    },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
