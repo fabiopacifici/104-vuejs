58ce8e33389778150a0742d2e20b41ed34a3865c
fbhood <mail@fabiopacifici.com>
2023-10-03 11:09:11 +0200

    add poster path with placeholder and stars logic

--- ex-boolflix/src/App.vue
+++ ex-boolflix/src/App.vue
@@ -19,7 +19,7 @@ export default {
       /*  const url_tv =  this.state.base_url + this.state.tv_endpoint + `?api_key=${this.state.api_key}&query=${this.state.query}`;
        this.state.fetchTV(url_tv) */
 
-       /* Option 2 - advanced */
+      /* Option 2 - advanced */
       this.state.fetchResults();
     },
     isFlag(lang) {
@@ -30,9 +30,13 @@ export default {
     },
     getFlagImage(lang) {
       return new URL(`./assets/img/${lang}.png`, import.meta.url).href
+    },
+    getPosterPath(name) {
+      return `https://image.tmdb.org/t/p/w342/${name}`
+    },
+    getVote(vote){
+      return Math.ceil(vote / 2)
     }
-
-
   },
 
   /* 
@@ -56,62 +60,16 @@ export default {
     Search
   </button>
 
-  <!-- Movies 
-  <h2>Movies</h2>
-  <ul>
-    <li v-for="movie in state.movies">
-      {{ movie.original_title }}
-      {{ movie.title }}
-
-       {{ movie.original_language }} 
-       <img :src="`https://flagsapi.com/${movie.original_language.toUpperCase()}/flat/64.png`" :alt="movie.original_language"> 
-       
-
-
-      <img :src="getFlagImage(movie.original_language)" alt="" v-if="isFlag(movie.original_language)">
-      <span v-else>
-        Lang: {{ movie.original_language }}
-      </span>
-
-
-
-      {{ movie.vote_average }}
-    </li>
-  </ul>
--->
-  <!-- TV
-  <h2>TV</h2>
-  <ul>
-    <li v-for="tv_show in state.tv">
-      {{ tv_show.original_name }}
-      {{ tv_show.name }}
-
-      {{ tv_show.original_language }}
-      <img :src="`https://flagsapi.com/${tv_show.original_language.toUpperCase()}/flat/64.png`" :alt="tv_show.original_language"> 
-
-
-
-      <img :src="getFlagImage(tv_show.original_language)" alt="" v-if="isFlag(tv_show.original_language)">
-      <span v-else>
-        Lang: {{ tv_show.original_language }}
-      </span>
-
-
-
-      {{ tv_show.vote_average }}
-    </li>
-  </ul>
- -->
-
-
-  <!-- Option 2 Movies/TV -->
-
-  <section v-for="(records, key) in state.results">
+  <section v-for="(records, key) in state.results" v-show="state.results.movies != ''">
     <h2>
       {{ key }}
     </h2>
 
     <div v-for="item in records.results">
+
+      <img class="poster" :src="getPosterPath(item.poster_path)" alt="" v-if="item.poster_path">
+      <img class="poster" width="342" src="./assets/img/poster_placeholder.png" v-else>
+
 
       <template v-if="key === 'movies'">
         {{ item.original_title }}
@@ -127,9 +85,29 @@ export default {
         Lang: {{ item.original_language }}
       </span>
 
-
-
       {{ item.vote_average }}
+
+      <template v-for="star in getVote(item.vote_average)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill"
+          viewBox="0 0 16 16">
+          <path
+            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+        </svg>
+      </template>
+
+      <template v-for="empty_star in 5 - getVote(item.vote_average)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star"
+          viewBox="0 0 16 16">
+          <path
+            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+        </svg>
+      </template>
+
+
+      <div class="overview">
+        {{ item.overview }}
+      </div>
+
 
     </div>
 
